220e3419b80757cc6c1f432a71dff50deef2c083
adaemmer <64265438+adaemmer@users.noreply.github.com>
2020-05-11 09:42:32 +0200

    add multi package support

--- offlinecopy.sh
+++ offlinecopy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Idea based on https://unix.stackexchange.com/a/489461
 
-if [ -z "$1" ]
+if [ -z "$@" ]
 then
  echo "Provide package to download, abort."
  exit
 fi
 
-PKG=$1
+PKG=$@
+PKGDIR= $(echo $PKG | tr " " "_"|head -c 20)
 releasever=7
 BASE_PATH=/tmp/offline
 INSTALL_PATH=$BASE_PATH/install
@@ -16,7 +17,7 @@ OFFLINE_REPO_PATH=/var/offlinerepo
 
 
 # Use separte installdir to load all dependencies, see https://unix.stackexchange.com/a/174485
-yum_output=$(yum install -q --downloadonly --installroot=$INSTALL_PATH/$PKG --releasever=$releasever --downloaddir=$DWL_PATH/$PKG $PKG 2>&1)
+yum_output=$(yum install -q --downloadonly --installroot=$INSTALL_PATH/$PKGDIR --releasever=$releasever --downloaddir=$DWL_PATH/$PKGDIR $PKG 2>&1)
 
 
 # Check if an error happened while downloading; if, exit script.
@@ -30,21 +31,21 @@ fi
 
 # Delete unnecessary data and create repo of downloaded packages
 rm -rf $INSTALL_PATH
-createrepo -q --database $DWL_PATH/$PKG
+createrepo -q --database $DWL_PATH/$PKGDIR
 
 
 # Create the repo-file
-echo "[offline-$PKG]
+echo "[offline-$PKGDIR]
 name=CentOS-$releasever - $PKG
-baseurl=file://$OFFLINE_REPO_PATH/$PKG
+baseurl=file://$OFFLINE_REPO_PATH/$PKGDIR
 enabled=0
 gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-$releasever" > $DWL_PATH/offline-$PKG.repo
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-$releasever" > $DWL_PATH/offline-$PKGDIR.repo
 
 
 # Output script end
-echo "Downloaded `ls -1 $DWL_PATH/$PKG | wc -l` files"
+echo "Downloaded `ls -1 $DWL_PATH/$PKGDIR | wc -l` files"
 echo ""
 echo "On offline-device: "
-echo " move yum-config to: \"/etc/yum.repos.d/offline-$PKG.repo\""
-echo " to install use: \"yum --disablerepo=\* --enablerepo=offline-$PKG install --nogpgcheck $PKG\""
+echo " move yum-config to: \"/etc/yum.repos.d/offline-$PKGDIR.repo\""
+echo " to install use: \"yum --disablerepo=\* --enablerepo=offline-$PKGDIR install --nogpgcheck $PKG\""
